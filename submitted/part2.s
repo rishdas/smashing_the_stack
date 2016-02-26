c79b874d3407fa4c5485f03533a21aaa4060fcdc
rishdas <sirshakdas@gmail.com>
2016-02-26 13:30:07 -0500

    modified version part2 with null termination

--- submitted/part2.s
+++ submitted/part2.s
@@ -1,6 +1,4 @@
 	.section .text
-.LC0:
-	.string "/bin/sh"		#The command to be executed
 	.global _start
 _start:
 	jmp     shell
@@ -14,4 +12,4 @@ exec:	popq    %rdi
 	leal    60(%r10d), %eax		#exit(0) system call
 	syscall
 shell:	call    exec			#to return the call to the exec call with return addr
-	.string "/bin/sh"		#The command to executed stored in return addr of call
+	.string "/bin/sh\0"		#The command to executed stored in return addr of call
